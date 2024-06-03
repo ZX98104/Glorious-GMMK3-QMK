58abf3341b0edc8718536fb4df6b70cf7a1bc639
Piotr Kleinschmidt <piotr.kleins@gmail.com>
2024-06-03 21:56:58 +0200

    gmmk3/p75/ansi/via: add full support for VIA

--- keyboards/gmmk/gmmk3/p75/ansi/keymaps/via/keymap.c
+++ keyboards/gmmk/gmmk3/p75/ansi/keymaps/via/keymap.c
@@ -36,24 +36,24 @@
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 enum custom_layers {
-    BASE,       // Base Layer
-    FUNCTION,   // Function Layer
-    CUSTOM,     // Custom Layer
+    _BL,   // Base Layer
+    _FL,   // Function Layer
+    _CL,   // Custom Layer
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layer (Default Layer) */
-    [BASE] = LAYOUT(
+    [_BL] = LAYOUT(
         KC_ESC,   KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,             KC_MUTE,
         KC_GRV,   KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
         KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
         KC_CAPS,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGDN,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-        KC_LCTL,  KC_LWIN, KC_LALT,                         KC_SPC,                  KC_RALT, MO(WIN_FL),         KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_LCTL,  KC_LWIN, KC_LALT,                         KC_SPC,                  KC_RALT, MO(_FL),         KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
  
     /* Function Layer */
-    [FUNCTION] = LAYOUT(
+    [_FL] = LAYOUT(
         _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
  
     /* Custom Layer */
-    [CUSTOM] = LAYOUT(
+    [_CL] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
@@ -72,3 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                   _______, _______,          _______, _______, _______
     ),
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_BL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_FL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_CL] = { ENCODER_CCW_CW(_______, _______)},
+};
+#endif // ENCODER_MAP_ENABLE
